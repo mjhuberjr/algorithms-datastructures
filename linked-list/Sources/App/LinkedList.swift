7095aae035fc6151c793d6b2a7b43a4548c6d290
Michael J. Huber Jr <info@op131.com>
2017-08-29 20:44:33 -0600

    Add addLast(value:) and addLast(node:) methods to LinkedList
    Also add passing tests for TwoNodesUsingAddFirstValueAndAddLastValue() also test of the LinkedList is empty.
    All tests are passing.

--- linked-list/Sources/App/LinkedList.swift
+++ linked-list/Sources/App/LinkedList.swift
@@ -2,8 +2,24 @@ import Foundation
 
 public class LinkedList<T> {
     
+    // MARK: - Properties
+    
     public var head: Node<T>?
     public var tail: Node<T>?
+    
+    public var count: Int {
+        if var node = head {
+            var count = 1
+            while let next = node.next {
+                node = next
+                count += 1
+            }
+            return count
+        } else {
+            return 0
+        }
+    }
+
     
     public init() { }
     
@@ -22,6 +38,21 @@ public class LinkedList<T> {
         if temp == nil {
             tail = head
         }
+    }
+    
+    public func addLast(value: T) {
+        let node = Node(value: value)
+        addLast(node: node)
+    }
+    
+    public func addLast(node: Node<T>?) {
+        if count == 0 {
+            head = node
+        } else {
+            tail?.next = node
+        }
+        
+        tail = node
     }
     
 }
--- linked-list/Tests/AppTests/SomeTests.swift
+++ linked-list/Tests/AppTests/SomeTests.swift
@@ -6,19 +6,40 @@ class SomeTests: XCTestCase {
     
     override func setUp() { }
     
-    func testHeadWithSingleNodeUsingAddValue() {
+    func testEmptyList() {
+        let linkedList = LinkedList<Int>()
+        XCTAssertEqual(0, linkedList.count, "This test failed because the linked list contains a node.")
+    }
+    
+    func testHeadAndTailWithSingleNodeUsingAddFirstValue() {
         let linkedList = LinkedList<Int>()
         linkedList.addFirst(value: 131)
         XCTAssertEqual(131, linkedList.head?.value, "This test failed because the head is not the correct value.")
         XCTAssertEqual(131, linkedList.tail?.value, "This test failed because the tail is not the correct value.")
     }
     
-    func testHeadWithSingleNodeUsingAddNode() {
+    func testHeadAndTailWithSingleNodeUsingAddFirstNode() {
         let linkedList = LinkedList<Int>()
         let node = Node(value: 131)
         linkedList.addFirst(node: node)
         XCTAssertEqual(131, linkedList.head?.value, "This test failed because the head is not the correct value.")
         XCTAssertEqual(131, linkedList.tail?.value, "This test failed because the tail is not the correct value.")
+    }
+    
+    func testHeadAndTailWithTwoNodesUsingAddFirstValue() {
+        let linkedList = LinkedList<Int>()
+        linkedList.addFirst(value: 1)
+        linkedList.addFirst(value: 13)
+        XCTAssertEqual(13, linkedList.head?.value, "This test failed because the head is not the correct value.")
+        XCTAssertEqual(1, linkedList.tail?.value, "This test failed because the tail is not the correct value.")
+    }
+    
+    func testHeadAndTailWithTwoNodesUsingAddFirstValueAndAddLastValue() {
+        let linkedList = LinkedList<Int>()
+        linkedList.addFirst(value: 13)
+        linkedList.addLast(value: 1)
+        XCTAssertEqual(13, linkedList.head?.value, "This test failed because the head is not the correct value.")
+        XCTAssertEqual(1, linkedList.tail?.value, "This test failed because the tail is not the correct value.")
     }
     
 }
